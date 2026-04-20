be13ebb335bafd47d912d3c6c1df85ef5a0ab601
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-04-19 21:46:33 -0300

    Add files via upload

--- Projeto Categoria Aula 10/src/main/java/com/example/crud/repository/ProdutoRepository.java
+++ Projeto Categoria Aula 10/src/main/java/com/example/crud/repository/ProdutoRepository.java
@@ -2,7 +2,9 @@ package com.example.crud.repository;
 
 import com.example.crud.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
